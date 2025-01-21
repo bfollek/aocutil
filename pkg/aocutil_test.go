fb734f4f71b4e8f7a5a78e04d163aa685556de30
bfollek <bfollek@gmail.com>
2025-01-21 11:27:51 -0500

    Remove testify.

--- pkg/aocutil_test.go
+++ pkg/aocutil_test.go
@@ -2,8 +2,6 @@ package aocutil
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 const testFile = "../testdata/file.txt"
@@ -18,15 +16,17 @@ func TestMustBitSliceToInt(t *testing.T) {
 }
 
 func TestMustFileToStrings(t *testing.T) {
-	got := MustFileToStrings(testFile)
 	wantI := 3
+	got := MustFileToStrings(testFile)
 	if len(got) != wantI {
 		t.Errorf("len(MustFileToStrings(%s)) = %d; want %d", testFile, len(got), wantI)
 	}
+
 	wantS := "111"
 	if got[0] != wantS {
 		t.Errorf("MustFileToStrings(%s) = %s; want %s", testFile, got[0], wantS)
 	}
+
 	wantS = "33333"
 	if got[2] != wantS {
 		t.Errorf("MustFileToStrings(%s) = %s; want %s", testFile, got[2], wantS)
@@ -34,26 +34,43 @@ func TestMustFileToStrings(t *testing.T) {
 }
 
 func TestMustFileToOneString(t *testing.T) {
-	got := MustFileToOneString(testFile)
 	want := "111\n2222\n33333"
+	got := MustFileToOneString(testFile)
 	if got != want {
 		t.Errorf("MustFileToStrings(%s) = %s; want %s", testFile, got, want)
 	}
 }
 
 func TestMustFileToInts(t *testing.T) {
-	require := require.New(t)
-	ii := MustFileToInts(testFile)
-	require.Equal(3, len(ii))
-	require.Equal(ii[0], 111)
-	require.Equal(ii[2], 33333)
+	want := 3
+	got := MustFileToInts(testFile)
+	if len(got) != want {
+		t.Errorf("len(MustFileToStrings(%s)) = %d; want %d", testFile, len(got), want)
+	}
+
+	want = 111
+	if got[0] != want {
+		t.Errorf("MustFileToStrings(%s) = %d; want %d", testFile, got[0], want)
+	}
+
+	want = 33333
+	if got[2] != want {
+		t.Errorf("MustFileToStrings(%s) = %d; want %d", testFile, got[2], want)
+	}
 }
 
 func TestSortStringInPlace(t *testing.T) {
-	require := require.New(t)
 	s := "f2HacbJELfls88"
-	s = SortString(s)
-	require.Equal("288EHJLabcffls", s)
-	s = SortString("X7m2ax9")
-	require.Equal("279Xamx", s)
+	want := "288EHJLabcffls"
+	got := SortString(s)
+	if got != want {
+		t.Errorf("SortString(%s)) = %s; want %s", s, got, want)
+	}
+
+	s = "X7m2ax9"
+	want = "279Xamx"
+	got = SortString(s)
+	if got != want {
+		t.Errorf("SortString(%s) = %s; want %s", s, got, want)
+	}
 }

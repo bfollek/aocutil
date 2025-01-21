e827389ced3a106ef05a2fe86b9710b9afb91414
bfollek <bfollek@gmail.com>
2025-01-21 10:04:51 -0500

    Work In Progress

--- pkg/aocutil_test.go
+++ pkg/aocutil_test.go
@@ -9,24 +9,36 @@ import (
 const testFile = "../testdata/file.txt"
 
 func TestMustBitSliceToInt(t *testing.T) {
-	require := require.New(t)
 	bits := []int{0, 1, 1, 0}
-	i := MustBitSliceToInt(bits)
-	require.Equal(i, 6)
+	want := 6
+	got := MustBitSliceToInt(bits)
+	if got != want {
+		t.Errorf("MustBitSliceToInt(%v) = %d; want %d", bits, got, want)
+	}
 }
 
 func TestMustFileToStrings(t *testing.T) {
-	require := require.New(t)
-	ss := MustFileToStrings(testFile)
-	require.Equal(3, len(ss))
-	require.Equal(ss[0], "111")
-	require.Equal(ss[2], "33333")
+	got := MustFileToStrings(testFile)
+	wantI := 3
+	if len(got) != wantI {
+		t.Errorf("len(MustFileToStrings(%s)) = %d; want %d", testFile, len(got), wantI)
+	}
+	wantS := "111"
+	if got[0] != wantS {
+		t.Errorf("MustFileToStrings(%s) = %s; want %s", testFile, got[0], wantS)
+	}
+	wantS = "33333"
+	if got[2] != wantS {
+		t.Errorf("MustFileToStrings(%s) = %s; want %s", testFile, got[2], wantS)
+	}
 }
 
 func TestMustFileToOneString(t *testing.T) {
-	require := require.New(t)
-	s := MustFileToOneString(testFile)
-	require.Equal(s, "111\n2222\n33333")
+	got := MustFileToOneString(testFile)
+	want := "111\n2222\n33333"
+	if got != want {
+		t.Errorf("MustFileToStrings(%s) = %s; want %s", testFile, got, want)
+	}
 }
 
 func TestMustFileToInts(t *testing.T) {
